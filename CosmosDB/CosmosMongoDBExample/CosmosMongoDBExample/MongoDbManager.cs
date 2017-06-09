1a7ea12960a656818706be075d3db10df9a198d4
ryuichi daigo <daigo@knowlbo.co.jp>
2017-06-10 03:00:21 +0900

    Azure Cosmos DB入門（4）サンプル追加

---
+++ CosmosDB/CosmosMongoDBExample/CosmosMongoDBExample/MongoDbManager.cs
@@ -0,0 +1,143 @@
+﻿// 一般的に利用する名前空間
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+
+// MongoDB関連の名前空間
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using MongoDB.Driver.Linq;
+
+namespace CosmosMongoDBExample
+{
+    public class MongoDbManager
+    {
+        private const string host = "[your Cosmos DB HOST]";
+        private const string userName = "[your UserName]";
+        private const string password = "[your Password]";
+        private const string database = "GroupwareDB";
+        private const string collection = "RoomReservations";
+        private const int port = 10255;
+
+        private MongoClient client = null;
+
+        public MongoDbManager()
+        {
+            // MongoClientの初期化
+            var settings = new MongoClientSettings
+            {
+                Server = new MongoServerAddress(MongoDbManager.host, MongoDbManager.port),
+                ServerSelectionTimeout = TimeSpan.FromSeconds(5)
+            };
+            settings.SslSettings = new SslSettings();
+            settings.UseSsl = true;
+            settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+            // ローカルMongoDBにオレオレ証明書で接続する場合は、以下のコメントはずす
+            //settings.SslSettings.CheckCertificateRevocation = false;
+            //settings.VerifySslCertificate = false;
+
+            MongoIdentity identity = new MongoInternalIdentity(MongoDbManager.database, MongoDbManager.userName);
+            MongoIdentityEvidence evidence = new PasswordEvidence(MongoDbManager.password);
+
+            settings.Credentials = new List<MongoCredential>()
+            {
+                new MongoCredential("SCRAM-SHA-1", identity, evidence)
+            };
+            this.client = new MongoClient(settings);
+        }
+
+        public async Task<bool> CreateCollection()
+        {
+            IMongoDatabase mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            await mongoDatabase.CreateCollectionAsync(MongoDbManager.collection);
+
+            return true;
+        }
+
+        public async Task<bool> CreateRoomReservationInfo(RoomReservationInfo roomReservationInfo)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+            await mongoCollection.InsertOneAsync(roomReservationInfo);
+
+            return true;
+        }
+
+        public async Task<RoomReservationInfo> FindByRoomAndId(string room, string id)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            var find = await mongoCollection.FindAsync<RoomReservationInfo>("{ Room: \"" + room + "\", _id: \"" + id + "\"}");
+
+            return find.FirstOrDefault();
+        }
+
+        public async Task<List<RoomReservationInfo>> FindByRoom(string room)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            var find = await mongoCollection.FindAsync<RoomReservationInfo>("{ Room: \"" + room + "\"}");
+
+            return find.ToList();
+        }
+
+        public async Task<List<RoomReservationInfo>> FindByRoomEx(string room)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            FilterDefinition<RoomReservationInfo> query = 
+                Builders<RoomReservationInfo>.Filter.Eq(r => r.Room, room);
+            var find = await mongoCollection.FindAsync<RoomReservationInfo>(query);
+
+            return find.ToList();
+        }
+
+        public async Task<List<RoomReservationInfo>> FindByRoomAndAssignMember(string room, string assignMemberId)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            var find = await mongoCollection.FindAsync<RoomReservationInfo>("{ Room: \"" + room + "\" , AssignMembers: { $elemMatch: { UserId: \"" + assignMemberId + "\"}}}");
+
+            return find.ToList();
+        }
+
+        public async Task<List<RoomReservationInfo>> FindByRoomLinq(string room)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            var find = await mongoCollection.FindAsync<RoomReservationInfo>(r => r.Room == room);
+
+            return find.ToList();
+        }
+
+        public async Task<bool> UpdateTitle(RoomReservationInfo roomReservationInfo)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            var result = await mongoCollection.UpdateOneAsync<RoomReservationInfo>(
+                r => r.Id == roomReservationInfo.Id && r.Room == roomReservationInfo.Room, 
+                new UpdateDefinitionBuilder<RoomReservationInfo>().Set(i => i.Title, roomReservationInfo.Title));
+
+            return true;
+        }
+
+        public async Task<bool> Delete(RoomReservationInfo roomReservationInfo)
+        {
+            var mongoDatabase = this.client.GetDatabase(MongoDbManager.database);
+            var mongoCollection = mongoDatabase.GetCollection<RoomReservationInfo>(MongoDbManager.collection);
+
+            var result = await mongoCollection.DeleteOneAsync<RoomReservationInfo>(
+                r => r.Id == roomReservationInfo.Id && r.Room == roomReservationInfo.Room);
+
+            return true;
+        }
+    }
+}
---
+++ CosmosDB/CosmosMongoDBExample/CosmosMongoDBExample/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Driver;
+
+namespace CosmosMongoDBExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MongoDbDbManagerクラスのhost / userName / password をあなたの環境の値に設定の上で実行してください。");
+            Console.WriteLine("また、1,000RUのコレクションを作成します。コレクションを作成すると、RUに比例したCosmos DB利用料金が課金されますのでご注意ください。");
+            Console.WriteLine("処理を続行して、よろしいですか？(y/n)");
+            if (Console.ReadLine().ToUpper() != "Y")
+            {
+                Console.WriteLine("処理を中断しました。");
+                return;
+            }
+
+            // 利用する機能をコメントアウトしてください。
+            //  データベース・コレクションの作成
+            //CreateCollection();
+            //  1件のドキュメントを作成
+            //CreateRoomReservationInfo();
+            //  Roomによる検索
+            //CallFindByRoom();
+            //  Roomによる検索
+            //CallFindByRoomEx();
+            //  RoomとAssignMemberによる検索
+            //CallFindByRoomAndAssignMember();
+            //  LINQでRoomよる検索
+            //CallFindByRoomLinq();
+            //  更新
+            //CallUpdate();
+            //  削除処理
+            //CallDelete();
+        }
+
+        public static void CreateCollection()
+        {
+            MongoDbManager manager = new MongoDbManager();
+            bool result = manager.CreateCollection().Result;
+        }
+
+        public static void CreateRoomReservationInfo()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            RoomReservationInfo roomReservationInfo = new RoomReservationInfo();
+            roomReservationInfo.Id = "A0000000001";
+            roomReservationInfo.Room = "第１会議室";
+            roomReservationInfo.ReservedUserId = "daigo";
+            roomReservationInfo.ReservedUserName = "醍醐竜一";
+            roomReservationInfo.Title = "MongoDB導入打ち合わせ";
+            roomReservationInfo.Start = new DateTime(2017, 7, 5, 10, 0, 0);
+            roomReservationInfo.End = new DateTime(2017, 7, 5, 12, 0, 0);
+            roomReservationInfo.AssignMembers = new List<AssignMember>();
+            roomReservationInfo.AssignMembers.Add(
+                new AssignMember() { UserId = "sakata", UserName = "坂田守" });
+
+            bool result = manager.CreateCollection().Result;
+        }
+
+        public static void CallFindByRoom()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            var roomReservationInfos = manager.FindByRoom("第１会議室").Result;
+
+            Console.WriteLine(string.Format("{0}件", roomReservationInfos.Count));
+            foreach (var info in roomReservationInfos)
+            {
+                Console.WriteLine(
+                    string.Format("{0} / {1} / {2} ～ {3}", info.Room, info.Title, info.Start, info.End));
+            }
+        }
+
+        public static void CallFindByRoomEx()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            var roomReservationInfos = manager.FindByRoomEx("第１会議室").Result;
+
+            Console.WriteLine(string.Format("{0}件", roomReservationInfos.Count));
+            foreach (var info in roomReservationInfos)
+            {
+                Console.WriteLine(
+                    string.Format("{0} / {1} / {2} ～ {3}", info.Room, info.Title, info.Start, info.End));
+            }
+        }
+
+        public static void CallFindByRoomAndAssignMember()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            var roomReservationInfos = manager.FindByRoomAndAssignMember("第１会議室", "daigo").Result;
+
+            Console.WriteLine(string.Format("{0}件", roomReservationInfos.Count));
+            foreach (var info in roomReservationInfos)
+            {
+                Console.WriteLine(
+                    string.Format("{0} / {1} / {2} ～ {3}", info.Room, info.Title, info.Start, info.End));
+            }
+        }
+
+        public static void CallFindByRoomLinq()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            var roomReservationInfos = manager.FindByRoomLinq("第１会議室").Result;
+
+            Console.WriteLine(string.Format("{0}件", roomReservationInfos.Count));
+            foreach (var info in roomReservationInfos)
+            {
+                Console.WriteLine(
+                    string.Format("{0} / {1} / {2} ～ {3}", info.Room, info.Title, info.Start, info.End));
+            }
+        }
+
+        public static void CallUpdate()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            var roomReservationInfo = manager.FindByRoomAndId("第１会議室", "0000000000").Result;
+
+            roomReservationInfo.Title = "タイトル変更！！！！";
+
+            var result = manager.UpdateTitle(roomReservationInfo).Result;
+        }
+
+        public static void CallDelete()
+        {
+            MongoDbManager manager = new MongoDbManager();
+
+            var roomReservationInfo = manager.FindByRoomAndId("第１会議室", "0000000001").Result;
+
+            var result = manager.Delete(roomReservationInfo).Result;
+        }
+    }
+}
